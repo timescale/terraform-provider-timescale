833238653c01cb298f1401242734019d3c20d601
Miquel Ruiz <miquel@timescale.com>
2025-05-13 10:46:11 +0200

    Update tooling

--- tools/tools.go
+++ tools/tools.go
@@ -1,8 +1,13 @@
-//go:build tools
+//go:build generate
 
 package tools
 
 import (
-	// Documentation generation
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
+
+// Format Terraform code for use in documentation.
+//go:generate terraform fmt -recursive ../examples/
+
+// Generate documentation.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name timescale

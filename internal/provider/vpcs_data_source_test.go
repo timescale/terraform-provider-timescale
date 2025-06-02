ccd82f4452ba171b170f56c9158a9240ecf4bc6e
Miquel Ruiz <miquel@timescale.com>
2025-06-02 14:50:23 +0200

    Fix tests

--- internal/provider/vpcs_data_source_test.go
+++ internal/provider/vpcs_data_source_test.go
@@ -14,7 +14,7 @@ func TestVPCDataSourceWithVPCPeering(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: providerConfig + testAccVPCDataSourceConfigWithVPCPeering(vpcName),
+				Config: providerConfig + testAccVPCDataSourceConfigWithVPCPeering(vpcName, peerAccountID, peerRegion, peerVPCID),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.id"),
@@ -31,9 +31,9 @@ func TestVPCDataSourceWithVPCPeering(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.vpc_id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.provisioned_id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.status"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_account_id", "000000000000"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_region_code", "us-west-2"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_vpc_id", "vpc-12345678"),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_account_id", peerAccountID),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_region_code", peerRegion),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_vpc_id", peerVPCID),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peering_type", "vpc"),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_cidr_blocks.#", "2"),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_cidr_blocks.0", "10.1.0.0/16"),
@@ -44,7 +44,7 @@ func TestVPCDataSourceWithVPCPeering(t *testing.T) {
 	})
 }
 
-func testAccVPCDataSourceConfigWithVPCPeering(vpcName string) string {
+func testAccVPCDataSourceConfigWithVPCPeering(vpcName, peerAccountId, peerRegion, peerVPCID string) string {
 	return fmt.Sprintf(`
 resource "timescale_vpcs" "test" {
   name        = %q
@@ -54,9 +54,9 @@ resource "timescale_vpcs" "test" {
 
 resource "timescale_peering_connection" "test" {
   timescale_vpc_id = timescale_vpcs.test.id
-  peer_account_id  = "000000000000"
-  peer_region_code = "us-west-2"
-  peer_vpc_id      = "vpc-12345678"
+  peer_account_id  = %q
+  peer_region_code = %q
+  peer_vpc_id      = %q
   peer_cidr_blocks = ["10.1.0.0/16", "10.2.0.0/16"]
 }
 
@@ -66,7 +66,7 @@ data "timescale_vpcs" "data_source" {
     timescale_peering_connection.test
   ]
 }
-`, vpcName)
+`, vpcName, peerAccountId, peerRegion, peerVPCID)
 }
 
 func TestVPCDataSourceWithTGWPeering(t *testing.T) {
@@ -76,7 +76,7 @@ func TestVPCDataSourceWithTGWPeering(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: providerConfig + testAccVPCDataSourceConfigWithTGWPeering(vpcName),
+				Config: providerConfig + testAccVPCDataSourceConfigWithTGWPeering(vpcName, peerAccountID, peerRegion, peerTGWID),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.id"),
@@ -93,9 +93,9 @@ func TestVPCDataSourceWithTGWPeering(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.vpc_id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.provisioned_id"),
 					resource.TestCheckResourceAttrSet("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.status"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_account_id", "000000000000"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_region_code", "us-west-2"),
-					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_vpc_id", "tgw-12345678"),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_account_id", peerAccountID),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_region_code", peerRegion),
+					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_tgw_id", peerTGWID),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peering_type", "tgw"),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_cidr_blocks.#", "2"),
 					resource.TestCheckResourceAttr("data.timescale_vpcs.data_source", "vpcs.0.peering_connections.0.peer_cidr_blocks.0", "10.1.0.0/16"),
@@ -106,7 +106,7 @@ func TestVPCDataSourceWithTGWPeering(t *testing.T) {
 	})
 }
 
-func testAccVPCDataSourceConfigWithTGWPeering(vpcName string) string {
+func testAccVPCDataSourceConfigWithTGWPeering(vpcName, peerAccountId, peerRegion, peerTGWID string) string {
 	return fmt.Sprintf(`
 resource "timescale_vpcs" "test" {
   name        = %q
@@ -116,9 +116,9 @@ resource "timescale_vpcs" "test" {
 
 resource "timescale_peering_connection" "test" {
   timescale_vpc_id = timescale_vpcs.test.id
-  peer_account_id  = "000000000000"
-  peer_region_code = "us-west-2"
-  peer_vpc_id      = "tgw-12345678"
+  peer_account_id  = %q
+  peer_region_code = %q
+  peer_tgw_id      = %q
   peer_cidr_blocks = ["10.1.0.0/16", "10.2.0.0/16"]
 }
 
@@ -128,5 +128,5 @@ data "timescale_vpcs" "data_source" {
     timescale_peering_connection.test
   ]
 }
-`, vpcName)
+	`, vpcName, peerAccountId, peerRegion, peerTGWID)
 }
